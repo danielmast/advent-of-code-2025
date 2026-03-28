127d8ff79693ec252548ad9de1857fb93521f15a
danielmast <danielmast92@gmail.com>
2026-03-28 20:30:48 +0100

    Add ruff

--- day1/solution.py
+++ day1/solution.py
@@ -1,4 +1,3 @@
-
 import logging
 from dataclasses import dataclass
 from enum import Enum
@@ -6,9 +5,11 @@ from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
+
 class Direction(Enum):
     LEFT = "L"
     RIGHT = "R"
+
 
 @dataclass
 class Rotation:
@@ -18,17 +19,18 @@ class Rotation:
     @classmethod
     def parse(cls, rotation_string: str) -> Rotation:
         return cls(
-            direction=Direction(rotation_string[0]),
-            distance=int(rotation_string[1:])
+            direction=Direction(rotation_string[0]), distance=int(rotation_string[1:])
         )
 
     def perform(self, dial: int) -> int:
         multiplier = -1 if self.direction == Direction.LEFT else 1
         return (dial + multiplier * self.distance) % 100
 
+
 def read_input() -> list[Rotation]:
-    with open(Path(__file__).parent / 'input.txt') as file:
+    with open(Path(__file__).parent / "input.txt") as file:
         return [Rotation.parse(line.strip()) for line in file]
+
 
 def solve(puzzle_input: list[Rotation]) -> int:
     zeroes = 0
@@ -48,8 +50,8 @@ def main():
 
     puzzle_input = read_input()
     answer = solve(puzzle_input)
-    logger.info(f'The answer is: {answer}')
+    logger.info(f"The answer is: {answer}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
