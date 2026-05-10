ec93450a74a74efa7dadf8a35eaf784c087b1f01
danielmast <danielmast92@gmail.com>
2026-05-10 22:12:00 +0200

    Refactor

--- day8/day8.py
+++ day8/day8.py
@@ -1,5 +1,6 @@
 import logging
 import math
+import sys
 from dataclasses import dataclass
 from functools import cache
 
@@ -30,7 +31,7 @@ class JunctionBox:
         return str(self.to_list())
 
     def __key(self):
-        return (self.x, self.y, self.z)
+        return self.x, self.y, self.z
 
     def __hash__(self):
         return hash(self.__key())
@@ -42,7 +43,7 @@ class JunctionBox:
 
 
 @dataclass(frozen=True)
-class Connection:
+class Distance:
     box1: JunctionBox
     box2: JunctionBox
 
@@ -53,7 +54,7 @@ class Connection:
     def __str__(self):
         return f"{self.box1} -> {self.box2} = {self.distance()}"
 
-    def __lt__(self, other: Connection) -> bool:
+    def __lt__(self, other: Distance) -> bool:
         return self.distance() < other.distance()
 
 
@@ -62,20 +63,21 @@ class Day8(Day[list[JunctionBox], int, int]):
         with open(self.input_path) as file:
             return [JunctionBox.parse(line.strip()) for line in file]
 
-    def get_lookup(self) -> dict[JunctionBox, set[JunctionBox]]:
+    def circuit_lookup(self) -> dict[JunctionBox, set[JunctionBox]]:
         lookup = {}
         for box in self.puzzle_input:
             lookup[box] = {box}
         return lookup
 
-    def distances(self) -> list[Connection]:
+    def sorted_distances(self) -> list[Distance]:
         distances = []
         for i, box_i in enumerate(self.puzzle_input):
             for box_j in self.puzzle_input[i + 1 :]:
-                distances.append(Connection(box_i, box_j))
+                distances.append(Distance(box_i, box_j))
+        distances.sort()
         return distances
 
-    def get_connection_count(self):
+    def connection_count(self):
         """
         Distinguish the number of connections that should be made
         between the example input and the actual input
@@ -84,46 +86,40 @@ class Day8(Day[list[JunctionBox], int, int]):
             return 10
         return 1000
 
+    def merge_circuits(self, limit: int) -> list[set[JunctionBox]] | int:
+        distances = self.sorted_distances()
+        lookup = self.circuit_lookup()
+
+        for i in range(limit):
+            merged = self.merge_circuits_inner(distances[i], lookup)
+
+            # Part 2
+            if len(merged) == len(self.puzzle_input):
+                return distances[i].box1.x * distances[i].box2.x
+
+        # Part 1
+        return [set(s) for s in set(frozenset(s) for s in lookup.values())]
+
+    def merge_circuits_inner(self, distance, lookup) -> set[JunctionBox]:
+        merged = lookup[distance.box1].union(lookup[distance.box2])
+        self.update_lookup(lookup, merged)
+        return merged
+
+    @staticmethod
+    def update_lookup(lookup, merged):
+        for box in merged:
+            lookup[box] = merged
+
     def solve_part1(self) -> int:
-        distances = self.distances()
-        distances.sort()
-
-        lookup = self.get_lookup()
-
-        for i in range(self.get_connection_count()):
-            box1 = distances[i].box1
-            box2 = distances[i].box2
-            circuit1 = lookup[box1]
-            circuit2 = lookup[box2]
-            merged = circuit1.union(circuit2)
-            for box in merged:
-                lookup[box] = merged
-
-        circuits = [set(s) for s in set(frozenset(s) for s in lookup.values())]
+        limit = self.connection_count()
+        circuits = self.merge_circuits(limit)
         largest_circuits = sorted(circuits, key=len, reverse=True)[:3]
         multiplied_sizes = math.prod(len(circuit) for circuit in largest_circuits)
-
         return multiplied_sizes
 
     def solve_part2(self) -> int:
-        distances = self.distances()
-        distances.sort()
-
-        lookup = self.get_lookup()
-
-        for i in range(len(distances)):
-            box1 = distances[i].box1
-            box2 = distances[i].box2
-            circuit1 = lookup[box1]
-            circuit2 = lookup[box2]
-            merged = circuit1.union(circuit2)
-            for box in merged:
-                lookup[box] = merged
-
-            if len(merged) == len(self.puzzle_input):
-                return box1.x * box2.x
-
-        raise AssertionError
+        limit = sys.maxsize
+        return self.merge_circuits(limit)
 
 
 def main():
